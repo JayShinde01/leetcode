0a73423c40d20b75a60197051734d0f97cbcd71b
Jay Anil Shinde <163715151+JayShinde01@users.noreply.github.com>
2026-09-14 22:55:39 +0530

    Solved LeetCode #13 - Roman to Integer

---
+++ 0013-roman-to-integer/RomanToInteger.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for(char ch : s.toCharArray()){
+            if(ch == '(' || ch == '[' || ch == '{')
+                stack.push(ch);
+            else{
+                if(stack.isEmpty())
+                    return false;
+                
+                    char last = stack.pop();
+                    if(last == '(' && ch != ')' ||
+                        last == '[' && ch != ']' ||
+                        last == '{' && ch != '}')return false;
+                
+            }
+        }
+        return stack.isEmpty();
+    }
+}

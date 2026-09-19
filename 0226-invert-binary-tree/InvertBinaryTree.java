2b869660091bc89bab28d10d21656046c2730c4c
Jay Anil Shinde <163715151+JayShinde01@users.noreply.github.com>
2026-09-19 22:19:02 +0530

    Updated LeetCode #226 - Invert Binary Tree

--- 0226-invert-binary-tree/InvertBinaryTree.java
+++ 0226-invert-binary-tree/InvertBinaryTree.java
@@ -14,7 +14,8 @@
  * }
  */
 class Solution {
-    public TreeNode invertTree(TreeNode root) {
-        
+    public int maxDepth(TreeNode root) {
+        if(root == null)return 0;
+        return 1 + Math.max(maxDepth(root.left),maxDepth(root.right));
     }
 }

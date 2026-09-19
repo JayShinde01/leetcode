257758588fde75e0e647bc5734450efcb386162e
Jay Anil Shinde <163715151+JayShinde01@users.noreply.github.com>
2026-09-19 22:44:40 +0530

    Solved LeetCode #501 - Find Mode in Binary Search Tree

---
+++ 0501-find-mode-in-binary-search-tree/FindModeInBinarySearchTree.java
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<String> binaryTreePaths(TreeNode root) {
+        
+    }
+    public void myFun(TreeNode root, List<String> list){
+        if(root == null)return;
+        
+        myFun(root.left,list);
+    }
+}

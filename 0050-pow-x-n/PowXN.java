e5389627f30c8698c5df7eea1d186cff1f82dea8
Jay Anil Shinde <163715151+JayShinde01@users.noreply.github.com>
2026-09-25 00:46:27 +0530

    Updated LeetCode #50 - Pow(x, n)

--- 0050-pow-x-n/PowXN.java
+++ 0050-pow-x-n/PowXN.java
@@ -1,20 +1,5 @@
 class Solution {
-    public boolean isValid(String s) {
-        Stack<Character> stack = new Stack<>();
-        for(char ch : s.toCharArray()){
-            if(ch == '(' || ch == '[' || ch == '{')
-                stack.push(ch);
-            else{
-                if(stack.isEmpty())
-                    return false;
-                
-                    char last = stack.pop();
-                    if(last == '(' && ch != ')' ||
-                        last == '[' && ch != ']' ||
-                        last == '{' && ch != '}')return false;
-                
-            }
-        }
-        return stack.isEmpty();
+    public double myPow(double x, int n) {
+        return Math.pow(x,n);
     }
 }
